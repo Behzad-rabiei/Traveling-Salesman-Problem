a9038be3125fda99e627e5cc094b4742358aca84
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2020-10-31 10:25:56 +0330

    Refactoring code

--- Individual/index.js
+++ Individual/index.js
@@ -6,12 +6,28 @@ class Individual  {  // Each individuals has cycle, cost of cycle, fitness
         this.cost =0;
         this.fitness= 0;    //  We have consider the fitness equal to 1/cost
     }
+    //  calling generate cycle and calculate cost of cycle
+    generate(){
+        this.generateCycle();
+        this.calculateCost();
+    }
+    //  Generating the cycle 
+    generateCycle(){
+        const nodeRange = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+        let temp, index;
+        for(let i=1; i<NODES; i++){
+            temp = nodeRange[Math.floor(Math.random() * nodeRange.length)];
+            this.cycle.push(temp);
+            for(let i=0; i<nodeRange.length; i++)   {
+                if(nodeRange[i]===temp) index=i;
+            }
+            nodeRange.splice(index,1);
+        }  
+    }
     //  Calculating the cost of cycle
-    calCost(){
-        //  Looping in cycle
-        //  start node --> preNode      next node --> nextNode
-        let preNode = 1;    
-        let nextNode = 1;   
+    calculateCost(){
+        let preNode = 1;    //  start node --> preNode
+        let nextNode = 1;   //  next node --> nextNode
         let cost = 0;
         for(let i=1; i<this.cycle.length; i++){
             preNode = nextNode;
@@ -23,35 +39,15 @@ class Individual  {  // Each individuals has cycle, cost of cycle, fitness
         }
         this.cost = cost;
         this.fitness = 1/cost;
+        console.log(this);
     }
 };
 
-//  Generating array of individual Objects
-function generateIndividuals(){              
-    for(let i=0; i<POPULATION; i++){
-        individuals.push(new Individual);
-        generateCycle(individuals[i]);
-    }
-}
-
-//  Geting a individual and generate cycle for that
-function generateCycle(individual){     
-    const nodeRange = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
-    let temp, index;
-    for(let i=1; i<NODES; i++){
-        temp = nodeRange[Math.floor(Math.random() * nodeRange.length)];
-        individual.cycle.push(temp);
-        for(let i=0; i<nodeRange.length; i++)   {
-            if(nodeRange[i]===temp) index=i;
-        }
-        nodeRange.splice(index,1);
-    }  
-}
-
-let individuals = []; 
-generateIndividuals();
-for(let i=0; i< individuals.length; i++){
-    individuals[i].calCost();
+//  generating array of Individuals
+let individuals = [];
+for(let i=0; i<POPULATION; i++){
+    individuals.push(new Individual);
+    individuals[i].generate();
 }
 
 module.exports = individuals;
